4d25ab7ad245e7887fa6de7322b97e27d69cda24
2000090139-Abhishek <ac9807623571@gmail.com>
2024-07-23 12:19:08 +0530

    # 4th commit

--- JJ_app/src/App.jsx
+++ JJ_app/src/App.jsx
@@ -9,11 +9,10 @@ const App=()=> {
     <div style={styles.container}>
       <h1 style={styles.heading}>Welcome to the Professor-Student Portal</h1>
       <p style={styles.paragraph}>This platform allows professors to upload notes and documents which students can download after logging in.</p>
-      {/* <div style={styles.links}>
+      <div style={styles.links}>
         <Link to="/about" style={styles.link}>About</Link>
-        <Link to="/login" style={styles.link}>Login</Link>
-        <Link to="/signup" style={styles.link}>Signup</Link>
-      </div> */}
+        
+      </div>
     </div>
   );
 };
--- JJ_app/src/main.jsx
+++ JJ_app/src/main.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from './App.jsx'
+import App from './App.jsx';
+import About from './About';
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+       
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
