8369f5431b276d2a77cc1b24f033e43709286f61
2000090139-Abhishek <ac9807623571@gmail.com>
2024-07-24 13:45:33 +0530

    #Sidebar and Header Updated

--- JJ_app/src/components/Sidebar.jsx
+++ JJ_app/src/components/Sidebar.jsx
@@ -1,34 +1,48 @@
-// src/components/Sidebar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-    <div className="d-flex flex-column flex-shrink-0 p-3 bg-light" style={{ width: '250px', height: '100vh' }}>
-      <ul className="nav nav-pills flex-column mb-auto">
-        <li className="nav-item">
-          <Link to="/" className="nav-link active" aria-current="page">
-            Home
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/about" className="nav-link">
-            About
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/login" className="nav-link">
-            Login
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/signup" className="nav-link">
-            Signup
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      {/* <button
+        className="btn btn-primary"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasSidebar"
+        aria-controls="offcanvasSidebar"
+      >
+        Toggle Sidebar
+      </button> */}
+
+      <div
+        className="offcanvas offcanvas-start"
+        id="offcanvasSidebar"
+        aria-labelledby="offcanvasSidebarLabel"
+      >
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title" id="offcanvasSidebarLabel">Menu</h5>
+          <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div className="offcanvas-body">
+          <ul className="nav flex-column">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/login" className="nav-link">Login</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/signup" className="nav-link">Signup</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
 export default Sidebar;
+
